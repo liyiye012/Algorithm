4fcc25f9172954dac92bc38ef8a8239e2ee4b410
liyiye012 <15652771941@163.com>
2018-08-15 14:39:26 +0800

    CodingInterview

--- Algorithm/06_ConstructBinaryTree.cpp
+++ Algorithm/06_ConstructBinaryTree.cpp
@@ -26,11 +26,8 @@ BinaryTreeNode* ConstructCore(int* startPreorder, int* endPreorder, int* startIn
 
 BinaryTreeNode* Construct(int* preorder, int* inorder, int length)
 {
-    if (preorder == nullptr || inorder == nullptr || length <= 0)
-        return nullptr;
-
-    return ConstructCore(preorder, preorder + length - 1,
-        inorder, inorder + length - 1);
+    if (preorder == NULL || inorder == NULL || length <= 0) return NULL;
+    return ConstructCore(preorder, preorder + length - 1, inorder, inorder + length - 1);
 }
 
 BinaryTreeNode* ConstructCore
@@ -39,43 +36,25 @@ BinaryTreeNode* ConstructCore
     int* startInorder, int* endInorder
 )
 {
-    // 前序遍历序列的第一个数字是根结点的值
-    int rootValue = startPreorder[0];
+    int rootvalue = startPreorder[0];
     BinaryTreeNode* root = new BinaryTreeNode();
-    root->m_nValue = rootValue;
-    root->m_pLeft = root->m_pRight = nullptr;
-
-    if (startPreorder == endPreorder)
-    {
-        if (startInorder == endInorder && *startPreorder == *startInorder)
-            return root;
-        else
-            throw std::exception("Invalid input.");
+    root->m_pLeft = NULL;
+    root->m_pRight = NULL;
+    if (startPreorder == endPreorder) {
+        if (startInorder == endInorder && *startPreorder == *startInorder) return root;
+        else throw std::exception("Invalid Input");
     }
-
-    // 在中序遍历中找到根结点的值
-    int* rootInorder = startInorder;
-    while (rootInorder <= endInorder && *rootInorder != rootValue)
-        ++rootInorder;
-
-    if (rootInorder == endInorder && *rootInorder != rootValue)
-        throw std::exception("Invalid input.");
-
-    int leftLength = rootInorder - startInorder;
-    int* leftPreorderEnd = startPreorder + leftLength;
-    if (leftLength > 0)
-    {
-        // 构建左子树
-        root->m_pLeft = ConstructCore(startPreorder + 1, leftPreorderEnd,
-            startInorder, rootInorder - 1);
+    int *rootInorder = startInorder;
+    while (rootInorder <= endInorder && *rootInorder != rootvalue) {
+        rootInorder++;
     }
-    if (leftLength < endPreorder - startPreorder)
-    {
-        // 构建右子树
-        root->m_pRight = ConstructCore(leftPreorderEnd + 1, endPreorder,
-            rootInorder + 1, endInorder);
-    }
-
+    if (rootInorder == endInorder && *rootInorder != rootvalue)
+        throw std::exception("Invalid Input");
+    int leftlen = rootInorder - startInorder;
+    int rightlen = endInorder - rootInorder;
+    int *leftPreorderEnd = startPreorder + leftlen;
+    if (leftlen > 0) root->m_pLeft = ConstructCore(startPreorder + 1, leftPreorderEnd, startInorder, rootInorder - 1);
+    if (rightlen > 0) root->m_pRight = ConstructCore(leftPreorderEnd + 1, endPreorder, rootInorder + 1, endInorder);
     return root;
 }
 
